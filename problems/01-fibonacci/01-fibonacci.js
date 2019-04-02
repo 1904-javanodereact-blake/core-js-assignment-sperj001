7f9fa34b143bfc64228f3ce9f10c69f915f97854
Sperj001 <josephsperduto@gmail.com>
2019-04-02 16:07:31 -0400

    Finished code

--- problems/01-fibonacci/01-fibonacci.js
+++ problems/01-fibonacci/01-fibonacci.js
@@ -1,6 +1,29 @@
-/* 1. Fibonacci
+/* . Fibonacci
 Define function: fib(n) 
 Return the nth number in the fibonacci sequence. */
-function fib(n) {
 
+let sequenceNumber = 5;
+
+function fib(n) {
+    switch(true){
+        case n==0 :
+            return  0;
+        case n==1 :
+            return 1;
+        case n==2 :
+            return 1;
+        case n>=3:
+            let ray = [];
+            ray[0] = 0;
+            ray[1] = 1;
+            ray[2] = 1;
+            let loco = 2;
+            for(i = 3; i <= n; i++){
+                ray[i] = ray[i-1] + ray[i-2];
+                loco++;
+            }
+            return ray[loco];
+    }
 }
+
+console.log(`The ${sequenceNumber} sequence number in the fibonacci sequence is: ${fib(sequenceNumber)}`);
