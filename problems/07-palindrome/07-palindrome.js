33c8641b5ddf23cac34d28042aa7256f9f5c05f8
Sperj001 <josephsperduto@gmail.com>
2019-04-02 19:52:37 -0400

    Finished Code

--- problems/07-palindrome/07-palindrome.js
+++ problems/07-palindrome/07-palindrome.js
@@ -1,6 +1,21 @@
 /* 7. Palindrome
 Define function isPalindrome(someStr)
 Return true if someStr is a palindrome, otherwise return false */
+let testMe = "drwho";
+
 function isPalindrome(someStr) {
-  
+    let splited = testMe.split("");
+    let reversed = "";
+    for(let i = 0; i<splited.length; i++){
+        reversed= reversed + splited[splited.length-1-i];
+    }
+    if(someStr == reversed){
+        return true;
+    }
+    else{
+        return false;
+    }
 }
+
+console.log(`The string ${testMe} is a Palindrome?
+Survey Says: ${isPalindrome(testMe)}`)
