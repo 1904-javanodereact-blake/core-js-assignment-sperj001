7989880ffd067da84e27a71b26a9934e3da39135
Sperj001 <josephsperduto@gmail.com>
2019-04-02 22:05:14 -0400

    Finished Code

--- problems/10-object-creation/10-object-creation.js
+++ problems/10-object-creation/10-object-creation.js
@@ -1,3 +1,14 @@
 /* 10 Object Creation
 Create an object using an object literal, constructor function, and es6 class.
 The object should have the fields name and age.*/
+class trainee {
+    constructor(name, age){
+        this.name = name;
+        this.age = age;
+    }
+    details(){
+         return `My name is ${this.name} and I am ${this.age} while participating in training.`;
+    }
+}
+let firstTrainee = new trainee("Joseph Sperduto", 21);
+console.log(firstTrainee.details());
