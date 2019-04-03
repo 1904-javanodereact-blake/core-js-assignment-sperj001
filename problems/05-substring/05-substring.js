d5f91eb88cf9361ee1a70e7a78f9508bcef743d1
Sperj001 <josephsperduto@gmail.com>
2019-04-03 15:24:46 -0400

    Finished Code

--- problems/05-substring/05-substring.js
+++ problems/05-substring/05-substring.js
@@ -6,12 +6,17 @@ For example substring('hello', 0, 3) should give 'hel' */
 let splitMe = "I am a good person";
 
 function substring(someStr, startIndex, endIndex) {
-    let cutString = someStr.split("");
-    let newString = "";
-    for(let i = (startIndex-1); i<endIndex; i++){
-        newString += cutString[i];
-    } 
-    return newString;
+    if(typeof(someStr) == "string"){
+        let cutString = someStr.split("");
+        let newString = "";
+        for(let i = (startIndex-1); i<endIndex; i++){
+            newString += cutString[i];
+        } 
+        return newString;
+    }
+    else{
+        return("Not a string inputed")
+    }
 }
 console.log(`Old string: ${splitMe} 
 New String: ${substring(splitMe, 8, 18)}`)
