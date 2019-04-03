263b55dbab9d9954996b7313c1bcbc74ec693877
Sperj001 <josephsperduto@gmail.com>
2019-04-02 21:15:04 -0400

    Finished Code

--- problems/08-shapes/08-shapes.js
+++ problems/08-shapes/08-shapes.js
@@ -21,5 +21,81 @@ Example for printShape("Diamond", 5, "*");
   * 
 */
 function printShape(shape, height, character) {
-  
+  if(shape == "square" || shape == "Square"){
+    for(let i = 0; i<height; i++){
+      let line = ""
+      for(let j = 0; j<=height; j++){
+        line = line+character;
+      }
+      console.log(line);
+    }
+  }
+  else if(shape == "triangle" || shape == "Triangle"){
+    let x = 1;
+    for(let i = 0; i<height; i++){
+      let line = ""
+      for(let j = 0; j<x; j++){
+        line = line+character;
+      }
+      x++;
+      console.log(line); 
+    } 
+  }
+  else if(shape == "diamond" || shape == "Diamond"){
+    let x = 0;
+    let y = 1;
+    let highend = 0;
+    let low = Math.floor((height/2));
+    let high = Math.ceil(height/2);
+    for(let i = 1; i<=height; i++){
+      let line = "";
+      if(x < low){
+        let linelow = Math.floor((height/2));
+        let linehigh = Math.ceil((height/2));
+        linelow -= x;
+        linehigh += x;
+        for(let j = 1; j<=height; j++){
+            if(j <= linelow){
+              line = line + " ";
+            }
+            else if(j > linehigh){
+              line = line + " ";
+            }
+            else{
+              line = line+character;
+            }
+        }
+      }
+      else if(x >= high){
+        let linelow = Math.floor((height/2));
+        let linehigh = Math.ceil((height/2));
+        highend = height-1 - x;
+        linelow -= highend;
+        linehigh += highend;
+        
+        for(let j = 1; j<height; j++){
+            if(j <= linelow){
+              line = line + " ";
+            }
+            else if(j > linehigh){
+              line = line + " ";
+            }
+            else{
+              line = line+character;
+            }
+        }
+        y++;
+      }
+      else{
+        for(let j = 0; j<height; j++){
+          line = line+character;
+        }
+      }
+      x++;
+      console.log(line);
+    }
+  }
 }
+printShape("square", 5, "*");
+printShape("triangle", 7, "^");
+printShape("diamond", 9, "@");
