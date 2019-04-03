c2368953766939eff3973db270b3e7b898d84779
Sperj001 <josephsperduto@gmail.com>
2019-04-03 09:07:05 -0400

    Finished Code

--- problems/11-splice-element/11-splice-element.js
+++ problems/11-splice-element/11-splice-element.js
@@ -2,6 +2,10 @@
 Define function spliceElement(someArr, index)
 Splice the element at the provided index in the array.
 */
+let spliceMe = ["1", "2", "3", "4", "5"];
 function spliceElement(someArr, index) {
-  
+    let oldArray = someArr.splice(index-1, 1); 
+    return someArr;
+        //splice (start index, amount to remove, to add)
 }
+console.log(spliceElement(spliceMe, 5));
