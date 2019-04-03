d75e710b6665ef78f59b63cc1263b3c29a23c204
Sperj001 <josephsperduto@gmail.com>
2019-04-02 21:52:42 -0400

    Finished Code

--- problems/09-object-properties/09-object-properties.js
+++ problems/09-object-properties/09-object-properties.js
@@ -1,6 +1,16 @@
 /* 9. Object Properties
 Define function objectProperties(someObj)
 Print every property and it's value. */
-function objectProperties(someObj) {
-
+class tangibleObject{
+    constructor(name, size, other) {
+        this.name = name;
+        this.size = size;
+        this.other = other;
+    }
 }
+function objectProperties(someObj) {
+    console.log(someObj);   
+}
+let objA = new tangibleObject("First Object", "Small", "Un-noteworthy");
+
+objectProperties(objA);
